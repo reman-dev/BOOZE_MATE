b7a515860c5f8a22860638be43c82e8c1269b8e5
DESKTOP-6POL0GV\haetj <haetji@gmail.com>
2022-05-24 11:00:03 +0900

    1st finish welsite

--- js/youtube.js
+++ js/youtube.js
@@ -11,11 +11,11 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 function onYouTubePlayerAPIReady() {
   // <div id="player"></div>
   new YT.Player('player', {
-    videoId: 'An6LvWQuj_8', // 최초 재생할 유튜브 영상 ID
+    videoId: 'Up18C-SZd2Y', // 최초 재생할 유튜브 영상 ID
     playerVars: {
       autoplay: true, // 자동 재생 유무
       loop: true, // 반복 재생 유무
-      playlist: 'An6LvWQuj_8' // 반복 재생할 유튜브 영상 ID 목록
+      playlist: 'Up18C-SZd2Y' // 반복 재생할 유튜브 영상 ID 목록
     },
     events: {
       // 영상이 준비되었을 때,
